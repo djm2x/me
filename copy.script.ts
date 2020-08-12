bf7a90c25f4d482a8bf9491e15d2677c5f5adcb0
djm2x <dj-m2x@hotmail.com>
2020-08-12 16:15:21 +0100

    me & you

--- copy.script.ts
+++ copy.script.ts
@@ -3,8 +3,10 @@ import * as fs from 'fs';
 async function main() {
   try {
     const content = await fs.promises.readFile(`${__dirname}/docs/index.html`, 'utf8');
+    const cname = await fs.promises.readFile(`${__dirname}/CNAME`, 'utf8');
 
     await fs.promises.writeFile(`${__dirname}/docs/404.html`, content);
+    await fs.promises.writeFile(`${__dirname}/docs/CNAME`, cname);
 
     console.log('404.html created/updated succesfully');
   } catch (error) {
