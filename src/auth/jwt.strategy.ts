4185b966f39a88482cb1e19e89ae73d42fcbaea7
mohamed-mourabit <mohamed.mourabit@digitransform.co>
2022-02-26 00:20:18 +0100

    correst secret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,10 +8,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.SECRETKEY,
+      secretOrKey: process.env.SECRETKEY || 'somethingyouwantwhichissecurewillworkk',
       // passReqToCallback: true,
       ignoreExpiration: false,
-      expiresIn: process.env.EXPIRESIN
+      expiresIn: process.env.EXPIRESIN  || '365d'
     });
   }
 
